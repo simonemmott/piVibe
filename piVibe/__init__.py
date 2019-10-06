a33e42cdc34dbbf551edb4c60dac7e676eddd92d
simon <simon.emmott@yahoo.co.uk>
2019-10-07 00:59:00 +0100

    hmmmm

--- piVibe/__init__.py
+++ piVibe/__init__.py
@@ -362,7 +362,7 @@ def random_mode():
         s_time = 0.2
         t_on = s_time*intensity
         t_off = 0.2 - t_on
-        steps = int(random.randint(10,100)*(0.4/intensity**2))
+        steps = int(random.randint(10,100)*(0.4/intensity))
         if on:
             toggle = False
             for i in range(steps):

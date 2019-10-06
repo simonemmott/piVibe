6e47ee120b08b6955cbaf9cdbfb93232d9a05e2b
simon <simon.emmott@yahoo.co.uk>
2019-10-06 21:40:38 +0100

    hmmm

--- app/api/vibe.py
+++ app/api/vibe.py
@@ -10,10 +10,22 @@ logger = logging.getLogger(__name__)
 def vibe_state(state):
     logger.debug('Set vibe state: {s}'.format(s=state))
     if state == 'ON':
-        logger.debug('Vibe ON')
+        logger.debug('Vibe: ON')
         vibe.on()
     if state == 'OFF':
-        logger.debug('Vibe OFF')
+        logger.debug('Vibe: OFF')
         vibe.off()
+    if state == 'LOW':
+        logger.debug('Vibe Mode: LOW')
+        vibe.low()
+    if state == 'MEDIUM':
+        logger.debug('Vibe Mode: MEDIUM')
+        vibe.medium()
+    if state == 'HIGH':
+        logger.debug('Vibe Mode: HIGH')
+        vibe.high()
+    if state == 'WAVES':
+        logger.debug('Vibe Mode: WAVES')
+        vibe.waves()
     return jsonify(vibe.to_dict())
     

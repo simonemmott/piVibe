0fe9083de72e468a136be3dbdc04c55c9b534588
simon <simon.emmott@yahoo.co.uk>
2019-10-06 23:00:31 +0100

    hmmm

--- piVibe/__init__.py
+++ piVibe/__init__.py
@@ -73,8 +73,8 @@ def mode(cls):
 @mode
 class VeryLow(Mode):
     states = [
-        (True, 0.05),
-        (False, 1.5)
+        (True, 0.02),
+        (False, 0.2)
     ]
     def __init__(self):
         self.gen = gen(Low.states)
@@ -82,8 +82,8 @@ class VeryLow(Mode):
 @mode
 class Low(Mode):
     states = [
-        (True, 0.1),
-        (False, 0.5)
+        (True, 0.05),
+        (False, 0.1)
     ]
     def __init__(self):
         self.gen = gen(Low.states)
@@ -91,8 +91,8 @@ class Low(Mode):
 @mode    
 class Medium(Mode):
     states = [
-        (True, 0.2),
-        (False, 0.5)
+        (True, 0.05),
+        (False, 0.05)
     ]
     def __init__(self):
         self.gen = gen(Medium.states)
@@ -100,8 +100,8 @@ class Medium(Mode):
 @mode    
 class High(Mode):
     states = [
-        (True, 0.3),
-        (False, 0.5)
+        (True, 0.1),
+        (False, 0.05)
     ]
     def __init__(self):
         self.gen = gen(High.states)
